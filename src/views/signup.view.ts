9ceb741f0b77862f18aafcb5abc11d210681f906
Kwin094 <62923495+Kwin094@users.noreply.github.com>
2020-04-02 15:22:44 -0700

    Update signup.view.ts

--- src/views/signup.view.ts
+++ src/views/signup.view.ts
@@ -13,35 +13,39 @@ export class SignupView {
     private title: HTMLElement;
 
     constructor() {
-        this.app = document.querySelector('#root');
+        this.app = document.getElementById('root')
+            const html=`
+            <div class = 'box'>
+                <img src="http://icon-library.com/images/blitz-icon/blitz-icon-18.jpg" alt="Blitz Icon" width="128" height="128">
+                <form id='Signup' method='post'>
+                     <h1>Clode Blitz Signup</h1>
+                <div>
+                    <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/user-512.png" alt="user Icon" width="20" height="20">
+                    <input name='name' type='text' placeholder='Name' />            
+                    </div>
+                <div>
+                     <img src="http://icons.iconarchive.com/icons/custom-icon-design/mono-general-2/512/mail-icon.png" alt="Mail Icon" width="20" height="20">
+                    <input name='email' type='text' placeholder='Email' />
+                 </div>
+                <div>
+                    <img src="https://cdn2.iconfinder.com/data/icons/flat-ui-icons-24-px/24/lock-24-512.png" alt="Lock Icon" width="20" height="20">
+                    <input name='password' type='password' placeholder='Password' />
+                </div>
+                <div>
+                    <button type='submit'>Let's Code!</button>
+                </div>
+            </div>
+        `;
+        this.app.innerHTML =html;
 
-        this.form = document.createElement('form');
-        this.inputName =
-            Object.assign(document.createElement('input'), 
-                { type: 'text', placeholder: 'Name' });
-        this.inputEmail =
-            Object.assign(document.createElement('input'), 
-                { type: 'text', placeholder: 'Email' });
-        this.inputPassword =
-            Object.assign(document.createElement('input'), 
-                { type: 'password', placeholder: 'Password' });
-        this.submitButton = 
-            Object.assign(document.createElement('button'), 
-                { type: 'submit', textContent: "Let's Code!" });
-        this.form.append(
-            this.inputName, 
-            this.inputEmail, 
-            this.inputPassword, 
-            this.submitButton);
-
-        this.title = document.createElement('h1');
-        this.title.textContent = 'Signup';
-        this.app.append(this.title, this.form);    
-
-        this.form.addEventListener('submit', event => {
-            event.preventDefault();
-            const { origin, pathname } = location;
-            location.replace(origin+pathname+'?page=home');
+        document.getElementById('Signup')
+            .addEventListener('submit', event => {
+                //event.preventDefault(); -- actually post is handy, no need for ajax call
+                const { origin, pathname } = location;
+                setTimeout(()=>{ 
+                    // timeout is temporary hack pending server auth implementation
+                    location.replace(origin+pathname+'?page=home');
+                })
         });
             
     //    this._temporaryAgeText = '';
